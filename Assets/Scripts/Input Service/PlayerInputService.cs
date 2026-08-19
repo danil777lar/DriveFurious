05bb2692d9c86c3b7afcb35a9b1930ba2a9bba8d
Danil Kolbasenko <danil777lar@gmail.com>
2026-08-19 18:59:57 -0300

    add touch input, add one more level

--- Assets/Scripts/Input Service/PlayerInputService.cs
+++ Assets/Scripts/Input Service/PlayerInputService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using Larje.Core;
+using Larje.Core.Services.UI;
 using ProjectConstants;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 using PlayerActions = InputSystem_Actions.PlayerActions;
@@ -11,7 +13,11 @@ using UIActions = InputSystem_Actions.UIActions;
 [BindService(typeof(PlayerInputService), typeof(InputService))]
 public class PlayerInputService : InputService
 {
-    public override Vector2 PlayerMovement => GetActions<PlayerActions>().Move.ReadValue<Vector2>();
+    [SerializeField] private RectTransformEvents _pointerEvents;
+
+    private float _touchInput;
+
+    public override Vector2 PlayerMovement => GetPlayerMovement();
     public override InputAction UIBack => GetActions<UIActions>().Cancel;
     public override InputAction UIDebug => null;
     public override InputAction PlayerRun => GetActions<PlayerActions>().Sprint;
@@ -28,4 +34,35 @@ public class PlayerInputService : InputService
         { typeof(PlayerActions), true },
         { typeof(UIActions), true },
     };
+
+    private void Start()
+    {
+        _pointerEvents.EventPointerDown += OnPointerDown;
+        _pointerEvents.EventPointerUp += OnPointerUp;
+    }
+
+    private void OnDestroy()
+    {
+        _pointerEvents.EventPointerDown -= OnPointerDown;
+        _pointerEvents.EventPointerUp -= OnPointerUp;
+    }
+
+    private void OnPointerDown(PointerEventData eventData)
+    {
+        _touchInput = eventData.position.x < Screen.width / 2f ? -1f : 1f;
+    }
+
+    private void OnPointerUp(PointerEventData eventData)
+    {
+        _touchInput = 0f;
+    }
+
+    private Vector2 GetPlayerMovement()
+    {
+        Vector2 movement = GetActions<PlayerActions>().Move.ReadValue<Vector2>();
+        movement.x += _touchInput;
+        movement = Vector2.ClampMagnitude(movement, 1f);
+
+        return movement;
+    }
 }
